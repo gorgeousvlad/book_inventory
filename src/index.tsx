1b8b5d7fe3ec3d85d34785c09f66173fb5abb145
Vlad <gorgeousvlad@gmail.com>
2019-01-12 05:52:35 -0800

    history added

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Router, Route, Link } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { configureStore } from './store/index';
 import { UserListContainer } from './components/user_list/container';
 import { UserContainer } from './components/user_profile/container';
+import { history } from './utils/history';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       <>
         <div><Link to='/list'>Show Profiles List</Link></div>
         <Route path='/list' component={UserListContainer}/>
